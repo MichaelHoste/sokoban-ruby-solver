54086cf50ea355bfefcf6c6defa34c67fdeaea1a
Michaël Hoste <michael.hoste@gmail.com>
2015-03-16 00:23:16 +0100

    node

---
+++ models/node.rb
@@ -0,0 +1,14 @@
+class Node
+
+  attr_reader :pusher_zone, :boxes_zone
+
+  def initialize(pusher_zone, boxes_zone)
+    @pusher_zone = pusher_zone
+    @boxes_zone  = boxes_zone
+  end
+
+  def solution?
+    # boxes_zone == solver.goals_zone
+  end
+
+end
