4fb2e235e8c66f5cc1664695c7f317240de134c7
Michaël Hoste <michael.hoste@gmail.com>
2015-03-22 14:03:48 +0100

    packs are testesd

---
+++ spec/models/pack_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Pack do
+  it '.initialize' do
+    pack = Pack.new('spec/support/files/level.slc')
+
+    pack.file_name.should    == 'level.slc'
+    pack.email.should        == 'test@gmail.com'
+    pack.url.should          == 'http://test.com'
+    pack.name.should         == 'Original & Extra'
+    pack.description.should  == 'The 50 original levels from Sokoban plus the 40 from Extra.'
+    pack.copyright.should    == 'Thinking Rabbit'
+    pack.max_cols.should     == 19
+    pack.max_rows.should     == 11
+    pack.levels.count.should == 1
+
+    pack.levels.first.to_s.should == "    #####          \n"\
+                                     "    #   #          \n"\
+                                     "    #$  #          \n"\
+                                     "  ###  $##         \n"\
+                                     "  #  $ $ #         \n"\
+                                     "### # ## #   ######\n"\
+                                     "#   # ## #####  ..#\n"\
+                                     "# $  $          ..#\n"\
+                                     "##### ### #@##  ..#\n"\
+                                     "    #     #########\n"\
+                                     "    #######        "\
+  end
+end
