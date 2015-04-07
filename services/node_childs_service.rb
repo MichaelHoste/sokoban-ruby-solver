255e27b18931a97b33e6740597d5d93562119708
Michaël Hoste <michael.hoste@gmail.com>
2015-04-07 23:17:32 +0200

    more tests and debug NodeChildsService

--- services/node_childs_service.rb
+++ services/node_childs_service.rb
@@ -13,81 +13,106 @@ class NodeChildsService
   end
 
   def run
-    childs = []
+    @child_levels = []
 
     reachable_boxes_zone = @pusher_zone & @boxes_zone
 
     @level.zone_pos_to_level_pos.each_pair do |zone_pos, level_pos|
       if reachable_boxes_zone.bit_1?(zone_pos)
-        [
-          {
-            :ahead_pos  => level_pos - 1,
-            :behind_pos => level_pos + 1
-          },
-          {
-            :ahead_pos  => level_pos + 1,
-            :behind_pos => level_pos - 1
-          },
-          {
-            :ahead_pos  => level_pos - @level.cols,
-            :behind_pos => level_pos + @level.cols
-          },
-          { :ahead_pos  => level_pos + @level.cols,
-            :behind_pos => level_pos - @level.cols
-          }
-        ].each do |move|
-          ahead_pos  = move[:ahead_pos]
-          behind_pos = move[:behind_pos]
+        ways_to_move_from(level_pos).each do |move|
+          ahead_pos      = move[:ahead_pos]
+          behind_pos     = move[:behind_pos]
+          ahead_zone_pos = @level.level_pos_to_zone_pos[ahead_pos]
 
-          not_wall                = @level.grid[ahead_pos] != '#'
-          correct_pusher_position = @pusher_zone.bit_1?(@level.level_pos_to_zone_pos[ahead_pos]) if not_wall
-          nothing_behind_box      = ['.', '@', '+', 's'].include?(@level.grid[behind_pos])
+          correct_pusher_position = @pusher_zone.bit_1?(ahead_zone_pos) if !ahead_zone_pos.nil?
+          pusher_position_not_box = !['$', '*'          ].include?(@level.grid[ahead_pos])
+          nothing_behind_box      =  ['.', '@', '+', 's'].include?(@level.grid[behind_pos])
 
-          if not_wall && correct_pusher_position && nothing_behind_box
+          if correct_pusher_position && pusher_position_not_box && nothing_behind_box
             new_level = @level.clone
 
-            # Remove box from old position
-            if new_level.grid[level_pos] == '*'
-              new_level.grid[level_pos] = '.'
-            else
-              new_level.grid[level_pos] = 's'
-            end
+            remove_box_from_old_position(new_level, level_pos)
 
-            # remove pusher from old position
-            old_pusher_m = new_level.pusher[:pos_m]
-            old_pusher_n = new_level.pusher[:pos_n]
+            remove_pusher_from_old_position(new_level)
 
-            if new_level.read_pos(old_pusher_m, old_pusher_n) == '+'
-              new_level.write_pos(old_pusher_m, old_pusher_n, '.')
-            else
-              new_level.write_pos(old_pusher_m, old_pusher_n, 's')
-            end
+            place_pusher_to_new_position(new_level, level_pos)
 
-            # place pusher to new position
-            if new_level.grid[level_pos] == '.'
-              new_level.grid[level_pos] = '+'
-            else
-              new_level.grid[level_pos] = '@'
-            end
+            add_box_to_new_position(new_level, behind_pos)
 
-            new_level.send(:initialize_pusher_position)
-
-            # Add box to new position
-            if new_level.grid[behind_pos] == '.'
-              new_level.grid[behind_pos] = '*'
-            else
-              new_level.grid[behind_pos] = '$'
-            end
-
-            childs << new_level
+            @child_levels << new_level
           end
         end
       end
     end
 
-    childs
+    self
+  end
+
+  def levels
+    @child_levels
+  end
+
+  def nodes
+    @child_nodes ||= @child_levels.collect { |level| Node.new(level) }
   end
 
   private
+
+  def ways_to_move_from(level_pos)
+    [
+      {
+        :ahead_pos  => level_pos - 1,
+        :behind_pos => level_pos + 1
+      },
+      {
+        :ahead_pos  => level_pos + 1,
+        :behind_pos => level_pos - 1
+      },
+      {
+        :ahead_pos  => level_pos - @level.cols,
+        :behind_pos => level_pos + @level.cols
+      },
+      { :ahead_pos  => level_pos + @level.cols,
+        :behind_pos => level_pos - @level.cols
+      }
+    ]
+  end
+
+  def remove_box_from_old_position(new_level, level_pos)
+    if new_level.grid[level_pos] == '*'
+      new_level.grid[level_pos] = '.'
+    else
+      new_level.grid[level_pos] = 's'
+    end
+  end
+
+  def remove_pusher_from_old_position(new_level)
+    old_pusher_m = new_level.pusher[:pos_m]
+    old_pusher_n = new_level.pusher[:pos_n]
+
+    if new_level.read_pos(old_pusher_m, old_pusher_n) == '+'
+      new_level.write_pos(old_pusher_m, old_pusher_n, '.')
+    else
+      new_level.write_pos(old_pusher_m, old_pusher_n, 's')
+    end
+  end
+
+  def place_pusher_to_new_position(new_level, level_pos)
+    if new_level.grid[level_pos] == '.'
+      new_level.grid[level_pos] = '+'
+    else
+      new_level.grid[level_pos] = '@'
+    end
+
+    new_level.send(:initialize_pusher_position)
+  end
+
+  def add_box_to_new_position(new_level, behind_pos)
+    if new_level.grid[behind_pos] == '.'
+      new_level.grid[behind_pos] = '*'
+    else
+      new_level.grid[behind_pos] = '$'
+    end
+  end
 
 end
--- spec/services/node_childs_service_spec.rb
+++ spec/services/node_childs_service_spec.rb
@@ -2,15 +2,231 @@ require 'spec_helper'
 
 describe NodeChildsService do
 
-  describe '#run', :focus => true do
-    level = Pack.new('spec/support/files/level.slc').levels[0]
-    node  = level.to_node
+  describe '#run (first level)' do
+    before :each do
+      level    = Pack.new('spec/support/files/level.slc').levels[0]
+      node     = level.to_node
+      @service = NodeChildsService.new(node).run
+    end
 
-    #puts node.to_s
+    it '#levels' do
+      child_levels = @service.levels
 
-    NodeChildsService.new(node).run.each do |child|
-      puts child.to_s
+      child_levels.count.should == 3
+      child_levels.collect(&:class).should == [Level, Level, Level]
+
+      child_levels[0].to_s.should == "    #####          \n"\
+                                     "    #   #          \n"\
+                                     "    #$  #          \n"\
+                                     "  ###  $##         \n"\
+                                     "  #  $$@ #         \n"\
+                                     "### # ## #   ######\n"\
+                                     "#   # ## #####  ..#\n"\
+                                     "# $  $          ..#\n"\
+                                     "##### ### # ##  ..#\n"\
+                                     "    #     #########\n"\
+                                     "    #######        "
+
+      child_levels[1].to_s.should == "    #####          \n"\
+                                     "    #   #          \n"\
+                                     "    #$  #          \n"\
+                                     "  ###  $##         \n"\
+                                     "  #  $ $ #         \n"\
+                                     "### # ## #   ######\n"\
+                                     "#   # ## #####  ..#\n"\
+                                     "# $ $@          ..#\n"\
+                                     "##### ### # ##  ..#\n"\
+                                     "    #     #########\n"\
+                                     "    #######        "
+
+      child_levels[2].to_s.should == "    #####          \n"\
+                                     "    #   #          \n"\
+                                     "    #$  #          \n"\
+                                     "  ###  $##         \n"\
+                                     "  #  $ $ #         \n"\
+                                     "### # ## #   ######\n"\
+                                     "#   #$## #####  ..#\n"\
+                                     "# $  @          ..#\n"\
+                                     "##### ### # ##  ..#\n"\
+                                     "    #     #########\n"\
+                                     "    #######        "
+    end
+
+    it '#nodes' do
+      child_nodes = @service.nodes
+
+      child_nodes.count.should == 3
+      child_nodes.collect(&:class).should == [Node, Node, Node]
+
+      child_nodes[0].to_s.should == "    #####          \n"\
+                                    "    #   #          \n"\
+                                    "    #$  #          \n"\
+                                    "  ###  $##         \n"\
+                                    "  #  $$@ #         \n"\
+                                    "### # ## #   ######\n"\
+                                    "#   # ## #####  ..#\n"\
+                                    "# $  $          ..#\n"\
+                                    "##### ### # ##  ..#\n"\
+                                    "    #     #########\n"\
+                                    "    #######        "
+
+      child_nodes[1].to_s.should == "    #####          \n"\
+                                    "    #   #          \n"\
+                                    "    #$  #          \n"\
+                                    "  ###  $##         \n"\
+                                    "  #  $ $@#         \n"\
+                                    "### # ## #   ######\n"\
+                                    "#   # ## #####  ..#\n"\
+                                    "# $ $           ..#\n"\
+                                    "##### ### # ##  ..#\n"\
+                                    "    #     #########\n"\
+                                    "    #######        "
+
+      child_nodes[2].to_s.should == "    #####          \n"\
+                                    "    #   #          \n"\
+                                    "    #$  #          \n"\
+                                    "  ###  $##         \n"\
+                                    "  #@ $ $ #         \n"\
+                                    "### # ## #   ######\n"\
+                                    "#   #$## #####  ..#\n"\
+                                    "# $             ..#\n"\
+                                    "##### ### # ##  ..#\n"\
+                                    "    #     #########\n"\
+                                    "    #######        "
     end
   end
 
+  describe '#run (special cases)' do
+    it '#run when destination is goal' do
+      text =  "#######\n"\
+              "#     #\n"\
+              "# @$. #\n"\
+              "#     #\n"\
+              "#######"
+
+      level    = Level.new(text)
+      node     = level.to_node
+      @service = NodeChildsService.new(node).run
+
+      child_levels = @service.levels
+
+      child_levels.count.should == 4
+
+      child_levels[0].to_s.should ==  "#######\n"\
+                                      "#     #\n"\
+                                      "#  @* #\n"\
+                                      "#     #\n"\
+                                      "#######"
+
+      child_levels[1].to_s.should ==  "#######\n"\
+                                      "#     #\n"\
+                                      "# $@. #\n"\
+                                      "#     #\n"\
+                                      "#######"
+
+      child_levels[2].to_s.should ==  "#######\n"\
+                                      "#     #\n"\
+                                      "#  @. #\n"\
+                                      "#  $  #\n"\
+                                      "#######"
+
+      child_levels[3].to_s.should ==  "#######\n"\
+                                      "#  $  #\n"\
+                                      "#  @. #\n"\
+                                      "#     #\n"\
+                                      "#######"
+    end
+
+    it '#run when box is on goal' do
+      text =  "#######\n"\
+              "# @   #\n"\
+              "#  *  #\n"\
+              "#  .  #\n"\
+              "#######"
+
+      level    = Level.new(text)
+      node     = level.to_node
+      @service = NodeChildsService.new(node).run
+
+      child_levels = @service.levels
+
+      child_levels.count.should == 4
+
+      child_levels[0].to_s.should ==  "#######\n"\
+                                      "#     #\n"\
+                                      "#  +$ #\n"\
+                                      "#  .  #\n"\
+                                      "#######"
+
+      child_levels[1].to_s.should ==  "#######\n"\
+                                      "#     #\n"\
+                                      "# $+  #\n"\
+                                      "#  .  #\n"\
+                                      "#######"
+
+      child_levels[2].to_s.should ==  "#######\n"\
+                                      "#     #\n"\
+                                      "#  +  #\n"\
+                                      "#  *  #\n"\
+                                      "#######"
+
+      child_levels[3].to_s.should ==  "#######\n"\
+                                      "#  $  #\n"\
+                                      "#  +  #\n"\
+                                      "#  .  #\n"\
+                                      "#######"
+    end
+
+    it '#run when other boxes on the way' do
+      text =  "#######\n"\
+              "#@    #\n"\
+              "# $$* #\n"\
+              "#     #\n"\
+              "#######"
+
+      level    = Level.new(text)
+      node     = level.to_node
+      @service = NodeChildsService.new(node).run
+
+      child_levels = @service.levels
+
+      child_levels.count.should == 6
+
+      child_levels[0].to_s.should ==  "#######\n"\
+                                      "#     #\n"\
+                                      "# @$* #\n"\
+                                      "# $   #\n"\
+                                      "#######"
+
+      child_levels[1].to_s.should ==  "#######\n"\
+                                      "# $   #\n"\
+                                      "# @$* #\n"\
+                                      "#     #\n"\
+                                      "#######"
+
+      child_levels[2].to_s.should ==  "#######\n"\
+                                      "#     #\n"\
+                                      "# $@* #\n"\
+                                      "#  $  #\n"\
+                                      "#######"
+
+      child_levels[3].to_s.should ==  "#######\n"\
+                                      "#  $  #\n"\
+                                      "# $@* #\n"\
+                                      "#     #\n"\
+                                      "#######"
+
+      child_levels[4].to_s.should ==  "#######\n"\
+                                      "#     #\n"\
+                                      "# $$+ #\n"\
+                                      "#   $ #\n"\
+                                      "#######"
+
+      child_levels[5].to_s.should ==  "#######\n"\
+                                      "#   $ #\n"\
+                                      "# $$+ #\n"\
+                                      "#     #\n"\
+                                      "#######"
+    end
+  end
 end
